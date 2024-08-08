d196461d83239a34b3cf35c0184a2f2a1a6281a7
Trex <trexbhaii@gmail.com>
2024-08-08 11:28:47 -0700

    Set Up Borders

---
+++ Assets/ScriptableObjects/Player Data.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+   void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
